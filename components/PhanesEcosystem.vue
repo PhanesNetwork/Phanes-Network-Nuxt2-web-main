6c348fa3d609c290f059eb7d26b97dc7c18d453f
Phanes Network <111890574+PhanesNetwork@users.noreply.github.com>
2024-02-03 15:30:57 +0300

    Update PhanesEcosystem.vue

--- components/PhanesEcosystem.vue
+++ components/PhanesEcosystem.vue
@@ -20,9 +20,9 @@
       data(){
           return{
               cards:[
-                { img:'cloud_db.png', title:'Staking Programme', snippet:'Utilizing a NFT-backed staking framework, Phanes Network offers a flexible decentralized staking pools across multiple exchange, enabling users to earn reward.',showAll:false},
+                { img:'cloud_db.png', title:'Staking Programme', snippet:'Utilizing a NFT-backed staking framework, Phanes Network offers a flexible decentralized staking pools across multiple chains, enabling creating an effortless reward accumulation.',showAll:false},
                 { img:'bag.png', title:'Phanes Index Portfolio', snippet:'Phanes Index Portfolio is designed to track performance of leading ecosystem assets, creating a strategic portfolio & investing management in the ecosystem, giving users diverse investment opportunities.', showAll:false},
-                { img: 'locker.png', title:'AMM / DEX & WALLET', snippet:" With our decentralised automated market maker, experience a flawless revolutionary DeFi solution that redefines the way users engage with liquidity provision and swapping of digital assets. For smooth asset control,  Phanes' final product will be a fiat-on-ramp integrated non-custodial wallet.",showAll:false},
+                { img: 'locker.png', title:'AMM / DEX & WALLET', snippet:"  For smooth asset control,  Phanes' final product will be a fiat-on-ramp integrated non-custodial wallet, integrating a decentralised multichain exchange, a flawless revolutionary DeFi solution that redefines the way users engage with digital assets management.",showAll:false},
             
             ]
         }
